e6f2dbf6f399bcb1e7312ea39dc455d0f1541589
Nanoic <2026256242@qq.com>
2024-03-28 17:49:19 +0800

    Client Base File
    基础代码

---
+++ Client/src/App.vue
@@ -0,0 +1,15 @@
+<!--
+ * @Author: Nanoic
+ * @LastEditors: Nanoic 2026256242@qq.com
+ * @Date: 2024-03-28 16:12:47
+ * @LastEditTime: 2024-03-28 17:17:53
+ * @FilePath: \Client\src\App.vue
+-->
+<script setup lang="ts"></script>
+
+<template>
+  <h1>PageHOME</h1>
+  <!-- <RouterView /> -->
+</template>
+
+<style scoped></style>
---
+++ Client/src/views/user/userLogin.vue
@@ -0,0 +1,15 @@
+<!--
+ * @Author: Nanoic
+ * @LastEditors: Nanoic 2026256242@qq.com
+ * @Date: 2024-03-28 17:23:22
+ * @LastEditTime: 2024-03-28 17:40:57
+ * @FilePath: \Client\src\views\user\userLogin.vue
+ * @Describe: 
+-->
+<script setup lang="ts"></script>
+
+<template>
+  <div>testa</div>
+</template>
+
+<script lang="ts"></script>
