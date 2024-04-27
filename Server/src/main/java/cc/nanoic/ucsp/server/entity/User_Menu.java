a4e500b43e5bb8d249c033f9f814f6fa1ce750ed
yrpyy <166906782+yrpyy@users.noreply.github.com>
2024-04-27 17:59:11 +0800

    【新增】菜单查询

--- Server/src/main/java/cc/nanoic/ucsp/server/entity/User_Menu.java
+++ Server/src/main/java/cc/nanoic/ucsp/server/entity/User_Menu.java
@@ -13,14 +13,14 @@ import java.sql.Date;
 
 @Data
 public class User_Menu {
-    private int id;
-    private int parent_id;
-    private String menu_name;
-    private String icon;
-    private String path;
-    private String level;
-    private String sort;
-    private String status;
-    private Date creat_time;
-    private Date update_time;
+    private Integer id;//用户唯一ID
+    private int parent_id;//该菜单的父级菜单id
+    private String menu_name;//该菜单的父级菜单id
+    private String icon;//该菜单的父级菜单id
+    private String path;//菜单路径
+    private String level;//菜单层级
+    private String sort;//菜单排序
+    private String status;//菜单状态(0/1)
+    private Date creat_time;//创建时间
+    private Date update_time;//更新时间
 }
