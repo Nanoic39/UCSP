2155b9caad73ed822cf494e433fe98a2734d025f
Nanoic <2026256242@qq.com>
2024-05-01 21:31:01 +0800

    【修复】修复后台管理系统菜单请求接口问题

--- Server/src/main/java/cc/nanoic/ucsp/server/mapper/AdminMapper.java
+++ Server/src/main/java/cc/nanoic/ucsp/server/mapper/AdminMapper.java
@@ -19,22 +19,22 @@ import java.util.List;
 public interface AdminMapper {
 
     //用户_角色关联查询
-    @Select("SELECT `id`, `user_id`, `role_id` FROM `user_role` WHERE `user_id` = #{id}")
+    @Select("SELECT `id`, `user_id`, `role_id` FROM user_role WHERE `user_id` = #{id}")
     List<User_Role> selectUserRoleById(@Param("id") Integer id);
 
     //角色查询
-    @Select("SELECT `id`, `name`, `intro`, `icon`, `color` FROM `role` WHERE `id` = #{role_id}")
+    @Select("SELECT `id`, `name`, `intro`, `icon`, `color` FROM role WHERE `id` = #{role_id}")
     Role selectUserRoleByRoleId(@Param("role_id") Integer id);
 
     //查询角色_权限关联表
-    @Select("SELECT `id`, `role_id`, `authority_id` FROM `role_authority` WHERE `id` = #{role_id}")
+    @Select("SELECT `id`, `role_id`, `authority_id` FROM role_authority WHERE `id` = #{role_id}")
     List<Role_Authority> selectRoleAuthorityByRoleId(@Param("role_id") Integer roleId);
 
     //查询权限
-    @Select("SELECT `id`,`level`,`intro` FROM `authority` WHERE `id` = (#{authorityId})")
+    @Select("SELECT `id`,`level`,`intro` FROM authority WHERE `id` = (#{authorityId})")
     Authority selectAuthorityByAuthorityId(@Param("authorityId") Integer authorityId);
 
     //查询菜单
-    @Select("SELECT * FROM menu WHERE `auth` = (#{authorityLevel})")
+    @Select("SELECT * FROM menu WHERE auth = #{authorityLevel}")
     Menu selectMenuByAuthorityLevel(@Param("authorityLevel") String authorityLevel);
 }
