93ae853b5cdeeb37db403c9295d6ea245e0978b7
fazhu4 <2641465728@qq.com>
2024-05-02 18:27:17 +0800

    优化

--- Server/src/main/java/cc/nanoic/ucsp/server/service/PostService.java
+++ Server/src/main/java/cc/nanoic/ucsp/server/service/PostService.java
@@ -16,15 +16,13 @@ public class PostService {
     public void insertPost(Post post) {//增加帖子
        Integer i=PostMapper.numSelect("帖子表");//i为当前帖子总数
         String p="post_"+(i/3000000+1);//利用帖子总数确定表数
-//        System.out.println(p);
+        CreateTableOnMethodCall createTableOnMethodCall = new CreateTableOnMethodCall();
 
-        if( i%3000000==0) {//如果新表的条数到300W建新表
-            CreateTableOnMethodCall createTableOnMethodCall = new CreateTableOnMethodCall();
-            createTableOnMethodCall.createTableOnMethodCall(i/3000000+1);
+        if(createTableOnMethodCall.TableName(p)) {//如果新表不存在则创建
+            createTableOnMethodCall.Table(i/3000000+1);
         }
         Integer max= PostMapper.numSelectMax(p);//拿到最新表的条数
         if (max==null) max=0;
-//        System.out.println(max);
         PostMapper.numUpdate((i/3000000)*3000000+max+1,"帖子表");//更新帖子总数
         PostMapper.insertPost(
                 p,
@@ -39,23 +37,66 @@ public class PostService {
     }
 
     public void delete(Post post) {//删除帖子
-        PostMapper.deletePost(post.getId());
+        String p="post_"+(post.getId()/3000000+1);
+        if(post.getId()%3000000==0){p="post_"+(post.getId()/3000000);}
+        Integer id=post.getId()%3000000;
+        if(id==0){id=3000000;}
+        PostMapper.deletePost(p,id);
+    }
+    public void studyDelete(Post post) {//删除学习区帖子
+        String p="studypost_"+(post.getId()/3000000+1);
+        if(post.getId()%3000000==0){p="studypost_"+(post.getId()/3000000);}
+        System.out.println(p);
+        Integer id=post.getId()%3000000;
+        if(id==0){id=3000000;}
+        PostMapper.deletePost(p,id);
     }
 
     public void update(Post post) {//更新帖子
-        int i= post.getId()/3000000;
-        String p="post_"+(i+1);
+        String p="post_"+(post.getId()/3000000+1);
+        if(post.getId()%3000000==0){p="post_"+(post.getId()/3000000);}
+        System.out.println(p);
+        Integer id=post.getId()%3000000;
+        if(id==0){id=3000000;}
+        System.out.println(id);
 
-        PostMapper.updatePost(p,post.getTitle(), post.getIntro(), post.getContent(), post.getPost_cover(),post.getId(),post.getUpdate_time());
+        PostMapper.updatePost(p,post.getTitle(), post.getIntro(), post.getContent(), post.getPost_cover(),id,post.getUpdate_time());
     }
 
+
     public void insertPost_study(Post_Study Post_Study) {//增加学习区帖子
-        PostMapper.insertPost_study(Post_Study.getTitle(),//文章标题
+        Integer i=PostMapper.numSelect("学习表");//i为当前帖子总数
+        String p="studypost_"+(i/3000000+1);//利用帖子总数确定表数
+        if( i%3000000==0) {//如果新表的条数到300W建新表
+            CreateTableOnMethodCall createTableOnMethodCall = new CreateTableOnMethodCall();
+            createTableOnMethodCall.studyTable(i/3000000+1);
+        }
+        Integer max= PostMapper.numSelectMax(p);//拿到最新表的条数
+        if (max==null) max=0;
+        PostMapper.numUpdate((i/3000000)*3000000+max+1,"学习表");//更新帖子总数
+
+        PostMapper.insertPost_study(
+                p,
+                Post_Study.getTitle(),//文章标题
                 Post_Study.getAuthor_id(),//作者ID
                 Post_Study.getIntro(),//摘要
                 Post_Study.getContent(),//文章正文
                 Post_Study.getPost_cover(),//文章封面
-                Post_Study.getTag() );//标签
+                Post_Study.getCreate_time(),
+                Post_Study.getUpdate_time(),
+                Post_Study.getTag() //标签
+        );
+    }
+
+    public void updatePost_study(Post_Study post) {//更新帖子
+        String p="studypost_"+(post.getId()/3000000+1);
+        if(post.getId()%3000000==0){p="studypost_"+(post.getId()/3000000);}
+        System.out.println(p);
+        Integer id=post.getId()%3000000;
+        if(id==0){id=3000000;}
+        System.out.println(id);
+
+        PostMapper.updatePost(p,post.getTitle(), post.getIntro(), post.getContent(), post.getPost_cover(),id,post.getUpdate_time());
     }
 
 }
