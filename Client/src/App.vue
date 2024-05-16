883f537f2d2ef106d3bf498f7f520d1967088d7f
Nanoic <2026256242@qq.com>
2024-05-16 23:09:43 +0800

    test1

--- Client/src/App.vue
+++ Client/src/App.vue
@@ -1,7 +1,15 @@
+<!--
+ * @Author: Nanoic
+ * @LastEditors: Nanoic 2026256242@qq.com
+ * @Date: 2024-05-16 06:13:54
+ * @LastEditTime: 2024-05-16 23:08:01
+ * @FilePath: \Client\src\App.vue
+ * @Describe: 
+-->
 <script setup></script>
 
 <template>
-  <router-view></router-view>
+  <router-view>222222</router-view>
 </template>
 
 <style scoped></style>
